9daea2aab13fdd585ec40728b22d2a4251522a67
Tanvir <tanvirhossain.butex@gmail.com>
2025-10-27 17:25:14 +0600

    feat(complain-issue): implement complain issue UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/home.css";
+import "../styles/complain_issue.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
