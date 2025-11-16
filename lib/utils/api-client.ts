078b2a693457623950b7dd3314a1625426004308
Tanvir <tanvirhossain.butex@gmail.com>
2025-11-16 12:59:10 +0600

    fix(api-client): update default API URL from localhost:3000 to localhost:5000

--- lib/utils/api-client.ts
+++ lib/utils/api-client.ts
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
 import { ApiError } from "../types/auth.types";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 
 interface RequestOptions extends RequestInit {
   skipAuth?: boolean;
